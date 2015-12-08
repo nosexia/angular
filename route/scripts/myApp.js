69d3dd193a0155acc9789f853633cbe55e537546
409903113@qq.com <409903113@qq.com>
2015-12-08 21:01:04 +0800

    注入内置服务$httpProvider

--- route/scripts/myApp.js
+++ route/scripts/myApp.js
@@ -1,14 +1,16 @@
 define([
     'angular',
     'controller/controllerModule',
-    'controller/detailController'
-],function(angular, controllerModule){
+    'controller/detailController',
+    'controller/listController'
+],function(angular, controllerModule, detailController, listController){
     //myApp模块依赖controllerModule模块
    var myApp = angular.module('myApp', ['controllerModule']);
-   //myApp.config中参数为数组，对应注入服务和函数
-   myApp.config(['$routeProvider', 'httpProvider', function($routeProvider, $httpProvider){
+   //注入内置服务$httpProvider
+   myApp.config(['$httpProvider', function($httpProvider){
         
-   }]) 
+   }]); 
+   console.log(myApp);
    //配置路由
    return myApp; 
 });

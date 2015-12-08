d08c911aa533b80ecf59e277fb65daea4a1a8dbe
409903113@qq.com <409903113@qq.com>
2015-12-08 20:52:16 +0800

    myApp.config中参数为数组，对应注入服务和函数

--- route/scripts/myApp.js
+++ route/scripts/myApp.js
@@ -5,5 +5,10 @@ define([
 ],function(angular, controllerModule){
     //myApp模块依赖controllerModule模块
    var myApp = angular.module('myApp', ['controllerModule']);
+   //myApp.config中参数为数组，对应注入服务和函数
+   myApp.config(['$routeProvider', 'httpProvider', function($routeProvider, $httpProvider){
+        
+   }]) 
+   //配置路由
    return myApp; 
 });

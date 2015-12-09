0915c57382f2e3fb31e917110fee7fa725a23f6b
409903113@qq.com <409903113@qq.com>
2015-12-09 11:06:04 +0800

    添加两个路由项，/detail, /list

--- route/scripts/myApp.js
+++ route/scripts/myApp.js
@@ -6,9 +6,15 @@ define([
     'angular-route'
 ],function(angular, controllerModule, detailController, listController, ngRoute){
    var myApp = angular.module('myApp', ['controllerModule', 'ngRoute']);
-   myApp.config(['$httpProvider', '$routeProvider', '$locationProvider', function($httpProvider, $routeProvider, $locationProvider){
-        //注入内置服务$locationProvider
-        console.log($locationProvider);
+   myApp.config(['$httpProvider', '$routeProvider', function($httpProvider, $routeProvider){
+        //添加两个路由项，'/detail', '/list'
+        $routeProvider.when('/detail',{
+            controller: 'detailController',
+            templateUrl: 'detailTpl'
+        }).when('/list',{
+            controller: 'listController',
+            templateUrl: 'listTpl'
+        });
    }]); 
    return myApp; 
 });

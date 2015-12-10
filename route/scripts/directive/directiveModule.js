41533e13b1e2793f3d22ba8383a80e408f195f2a
409903113@qq.com <409903113@qq.com>
2015-12-10 11:34:45 +0800

    myApp模块依赖directiveModule模块

---
+++ route/scripts/directive/directiveModule.js
@@ -0,0 +1,6 @@
+define([
+    'angular'
+],function(angular){
+    var directiveModule = angular.module('directiveModule', []);
+    return directiveModule;
+});
--- route/scripts/myApp.js
+++ route/scripts/myApp.js
@@ -3,12 +3,12 @@ define([
     'controller/controllerModule',
     'controller/detailController',
     'controller/listController',
+    'directive/directiveModule',
     'angular-route'
-],function(angular, controllerModule, detailController, listController, ngRoute){
-   var myApp = angular.module('myApp', ['controllerModule', 'ngRoute']);
+],function(angular, controllerModule, detailController, listController, directiveModule, ngRoute){      
+   var myApp = angular.module('myApp', ['controllerModule', 'directiveModule', 'ngRoute']);         //myApp模块依赖directiveModule模块
    myApp.config(['$httpProvider', '$routeProvider', function($httpProvider, $routeProvider){
-        //添加两个路由项，'/detail', '/list'
-        $routeProvider.when('/detail',{
+        $routeProvider.when('/',{
             controller: 'detailController',
             templateUrl: 'detailTpl'
         }).when('/list',{

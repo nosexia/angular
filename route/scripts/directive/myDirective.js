6b6cbcdefa83143705320e985bdfd21446b10196
409903113@qq.com <409903113@qq.com>
2015-12-10 14:40:39 +0800

    指令参数restrict，template

--- route/scripts/directive/myDirective.js
+++ route/scripts/directive/myDirective.js
@@ -1,4 +1,12 @@
 define([
-    'jquery'
-],function($){
-})
+    './directiveModule'
+],function(module){
+    module.directive('myDirective', function(){
+        //指令参数restrict, 用来指定指令的使用类型，默认是标签(A)的方式
+        //参数template:用来指定指令模版
+        return {
+            restrict: 'A',
+            template: '<div class="name">nose</div>'      
+        };
+    });
+});
--- route/scripts/myApp.js
+++ route/scripts/myApp.js
@@ -1,20 +1,9 @@
 define([
     'angular',
-    'controller/controllerModule',
-    'controller/detailController',
-    'controller/listController',
     'directive/directiveModule',
-    'angular-route'
-],function(angular, controllerModule, detailController, listController, directiveModule, ngRoute){      
-   var myApp = angular.module('myApp', ['controllerModule', 'directiveModule', 'ngRoute']);         //myApp模块依赖directiveModule模块
-   myApp.config(['$httpProvider', '$routeProvider', function($httpProvider, $routeProvider){
-        $routeProvider.when('/',{
-            controller: 'detailController',
-            templateUrl: 'detailTpl'
-        }).when('/list',{
-            controller: 'listController',
-            templateUrl: 'listTpl'
-        });
-   }]); 
+    'directive/myDirective'
+],function(angular, directiveModule, myDirective){      
+    //myApp模块依赖directiveModule模块
+   var myApp = angular.module('myApp', ['directiveModule']);     
    return myApp; 
 });

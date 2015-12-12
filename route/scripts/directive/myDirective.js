cf2482476b3e6a40a2a122da26d243a274b9f4c9
409903113@qq.com <409903113@qq.com>
2015-12-12 09:45:34 +0800

    ng-repeat-start指令，可以解析包含指令本身的标签

--- route/scripts/directive/myDirective.js
+++ route/scripts/directive/myDirective.js
@@ -2,10 +2,11 @@ define([
     './directiveModule'
 ],function(module){
     module.directive('myDirective', function(){
-        //ng-repeat指令 只能解析指令里面的内容
+        //ng-repeat-start指令，可以解析包含指令本身的标签
+        //ng-repeat-start和 ng-repeat-end是已一对标签出现的形式
         return {
             restrict: 'A',
-            template: '<div ng-repeat="item in testArray">{{item}}</div>',
+            template: '<div ng-repeat-start="item in testArray" ng-repeat-end value="{{item}}" ></div>',
             replace: true,
             scope: {
                 myName: '@',

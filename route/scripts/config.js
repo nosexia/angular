887f4a460aa3e2278538d5a185addaf7a8709afd
409903113@qq.com <409903113@qq.com>
2015-12-16 16:35:27 +0800

    exports:moment

--- route/scripts/config.js
+++ route/scripts/config.js
@@ -3,12 +3,18 @@ var require = {
     urlArgs: 'v=' + new Date().getTime(),
     paths: {
         angular: 'angular',
-        'angular-route': 'angular-route.min'
+        'angular-route': 'angular-route.min',
+        'moment': 'moment-2.8.1-with-locale'
     },
     shim: {
         angular:{
             exports: 'angular'
         },
+
+        moment:{
+            exports: 'moment'
+        },
+
         'angular-route': ['angular']
 
     }
--- route/scripts/myApp.js
+++ route/scripts/myApp.js
@@ -1,13 +1,17 @@
 define([
     'angular',
     'directive/directiveModule',
-    'directive/myDirective'
-],function(angular, directiveModule, myDirective){      
+    'directive/myDirective',
+    'moment'
+],function(angular, directiveModule, myDirective, moment){      
     //myApp模块依赖directiveModule模块
    var myApp = angular.module('myApp', ['directiveModule']); 
    myApp.controller('myController', ['$scope', function($scope){
         $scope.myAge = '23';
         $scope.isActive = true;
    }]);
+
+   console.log(moment);
+
    return myApp; 
 });

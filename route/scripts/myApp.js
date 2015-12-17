3b7fe0af9d0d294843da1981824495add38d77ad
409903113@qq.com <409903113@qq.com>
2015-12-17 20:43:09 +0800

    引入$sce内部服务，把字符串转为信任的html

--- route/scripts/myApp.js
+++ route/scripts/myApp.js
@@ -5,9 +5,9 @@ define([
     'filter/filterModule',
     'filter/momentFilter'
 ],function(angular, directiveModule, myDirective, filterModule, momentFilter){      
-    //引入$sce服务，把字符串转为信任的html
+    //引入$sce内部服务，把字符串转为信任的html
    var myApp = angular.module('myApp', ['directiveModule', 'filterModule']); 
-   myApp.controller('myController', ['$scope', '$sce', function($scope, sce){
+   myApp.controller('myController', ['$scope', '$sce', function($scope, $sce){
     $scope.myAge = sce.trustAsHtml('<a href="https://www.baidu.com">百度一下</a>');
         $scope.isActive = true;
    }]);

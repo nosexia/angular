f87fc60f50a267395c9d309d515db2186dcf6f88
nose <nose@futunn.com>
2016-03-02 16:47:54 +0800

    定义一个模块需要有两个参数“模块名”, “依赖数组”

--- route/scripts/myApp.js
+++ route/scripts/myApp.js
@@ -5,7 +5,7 @@ define([
     'filter/filterModule',
     'filter/momentFilter'
 ],function(angular, directiveModule, myDirective, filterModule, momentFilter){      
-    //引入$sce内部服务，把字符串转为信任的html
+    //定义一个模块需要有两个参数'模块名', '依赖数组'
    var myApp = angular.module('myApp', ['directiveModule', 'filterModule']); 
    myApp.controller('myController', ['$scope', '$sce', function($scope, $sce){
         $scope.myAge = $sce.trustAsHtml('<a href="https://www.baidu.com">百度一下</a>');

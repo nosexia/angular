fa25a262343452abcf089a41d535a2a8a11c2cc5
nose <nose@futunn.com>
2016-03-02 16:46:09 +0800

    '“myApp”是定义在myApp.js中的模块名

--- route/scripts/appMain.js
+++ route/scripts/appMain.js
@@ -1,7 +1,7 @@
 require([
     'angular',
     'myApp'    
-],function(angular, myApp){  
-    //myApp模块解析html中body
+],function(angular){  
+    //'myApp'是定义在myApp.js中的模块名
     angular.bootstrap(document.body, ['myApp']);
 });
--- route/scripts/myApp.js
+++ route/scripts/myApp.js
@@ -8,9 +8,8 @@ define([
     //引入$sce内部服务，把字符串转为信任的html
    var myApp = angular.module('myApp', ['directiveModule', 'filterModule']); 
    myApp.controller('myController', ['$scope', '$sce', function($scope, $sce){
-    $scope.myAge = sce.trustAsHtml('<a href="https://www.baidu.com">百度一下</a>');
+        $scope.myAge = $sce.trustAsHtml('<a href="https://www.baidu.com">百度一下</a>');
         $scope.isActive = true;
    }]);
 
-   return myApp; 
 });

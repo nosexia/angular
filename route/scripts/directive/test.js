b45691113f808139794f5c13c19f908b89a865c1
nose <nose@futunn.com>
2016-03-02 17:02:23 +0800

    自定义指定，直接返回函数

---
+++ route/scripts/directive/test.js
@@ -0,0 +1,9 @@
+define([],function(){
+    // 自定义指定，直接返回函数
+    return function(){
+        return {
+            restrict: 'EA',
+            template: '<div>1</div>'
+        };
+    };
+});
--- route/scripts/myApp.js
+++ route/scripts/myApp.js
@@ -2,14 +2,17 @@ define([
     'angular',
     'directive/directiveModule',
     'directive/myDirective',
+    'directive/test',
     'filter/filterModule',
     'filter/momentFilter'
-],function(angular, directiveModule, myDirective, filterModule, momentFilter){      
+],function(angular, directiveModule, myDirective, test, filterModule, momentFilter){      
     //定义一个模块需要有两个参数“模块名”, “依赖数组”
    var myApp = angular.module('myApp', ['directiveModule', 'filterModule']); 
    myApp.controller('myController', ['$scope', '$sce', function($scope, $sce){
         $scope.myAge = $sce.trustAsHtml('<a href="https://www.baidu.com">百度一下</a>');
         $scope.isActive = true;
    }]);
+
+   myApp.directive('test', test);
 
 });

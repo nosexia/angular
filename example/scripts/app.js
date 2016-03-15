96bad8d82b69ff5a1db1b5b5797e2a925423e04f
nose <nose@futunn.com>
2016-03-15 14:43:39 +0800

    myApp.run方法自动执行

--- example/scripts/app.js
+++ example/scripts/app.js
@@ -4,4 +4,9 @@ define([
 ], function(angular, indexController){
     var myApp = angular.module('myApp', []);
     myApp.controller('indexController', indexController);
+
+    // myApp.run 自动跑run方法
+    myApp.run(['$rootScope', function($rootScope){
+        console.log('开始跑了噢');
+    }]);
 });

31a1854c736fffa7d5f76122ebda06ddfd298c40
nose <nose@futunn.com>
2016-03-15 10:22:02 +0800

    在引用angular时, 文件名必须是angular.js

---
+++ example/scripts/app.js
@@ -0,0 +1,7 @@
+define([
+    'angular',
+    'controllers/indexController'
+], function(angular, indexController){
+    var myApp = angular.module('myApp', []);
+    myApp.controller('indexController', indexController);
+});
---
+++ example/scripts/main.js
@@ -0,0 +1,6 @@
+require([
+    'angular',
+    'app'
+],function(angular){
+    angular.bootstrap(document, 'myApp');
+});

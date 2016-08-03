f796ac89ade1ea7de29e6c832c3b8b570c23aa17
nose <nose@futunn.com>
2016-08-03 16:21:28 +0800

    $watch加入第三个参数时，必须全等时，才不执行回调（值相等，引用相同）

--- example/scripts/controllers/indexController.js
+++ example/scripts/controllers/indexController.js
@@ -3,5 +3,11 @@ define([
 ], function(){
     return ['$scope', function($scope){
         $scope.highLight = 1;
+        $scope.update = 0;
+        // $watch加入第三个参数时，必须全等时，才不执行回调（值相等，引用相同）
+        $scope.$watch('user', function(newValue, oldValue){
+            if(newValue === oldValue) return;
+            $scope.update ++;
+        },true);
     }];
 });

dcffd924927b6d9d1dc870ab6a996eebf18f3149
nose <nose@futunn.com>
2016-09-26 15:51:20 +0800

    通过myApp.config可以注入一个服务类， 返回$httpProvider的实例

--- example/scripts/app.js
+++ example/scripts/app.js
@@ -6,6 +6,7 @@ define([
 ], function(angular, init, indexController){
     var myApp = angular.module('myApp', ['moduleService']);
     myApp.controller('indexController', indexController);
+    // 通过myApp.config可以注入一个服务类， 返回$httpProvider的实例
     myApp.config(['$httpProvider', function($httpProvider){
         init.modifyHttpRequest($httpProvider);
     }]);
